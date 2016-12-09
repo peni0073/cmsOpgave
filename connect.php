90c2622cd863b70179736ec0690385d7812dfbd2
peni0073 <peni0073@edu.campusvejle.dk>
2016-12-09 08:59:15 +0100

    Tilføjede mapper til css, fonts, img og js og oprettede index.php, connect.php og custom.css

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php 
+$host = "localhost";
+$dbName = "cmsopgave";
+$dbUsername = "root";
+$dbPassword = "";
+
+try{
+$DBH = new PDO("mysql:dbname=$dbName;host=$host;charset=utf8", $dbUsername, dbPassword, array (PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf9") );
+$DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch{(PDOException $e){
+    echo "Noget gik galt!: <br>";
+    echo $e->getMessage( );
+}
+    
+}
+?>
---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>CMS - Tattoos</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <body>
+    <!-- HEADER START -->
+    <div class="container">
+        <header class="row">
+            <div class="col-xs-12 image">   
+                <img src="img/header-img.jpg" alt="Tattoos header">            
+            </div>
+        </header>
+    </div>
+    <!-- HEADER SLUT -->
+
+    <!-- NAVIGATION START -->
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <nav class="navbar navbar-default">
+                  <div class="container-fluid">                   
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                      <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Forside <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">Kontakt</a></li>                        
+                      </ul>
+
+                      <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Log ind</a></li>                        
+                      </ul>
+                    </div><!-- /.navbar-collapse -->
+                  </div><!-- /.container-fluid -->
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- NAVIGATION SLUT -->  
+    
+    <!-- MAIN START -->
+    <div class="container">
+        <main class="row">
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-sm-8">
+                        <article>                          
+                            <img src="img/img4.jpg" alt="Tattoo billede 1">
+                            <h3>Overskrift</h3>
+                            <p id="time"><?php $t=time(); echo(date("Y-m-d",$t)); ?>
+                            </p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, fuga. Reprehenderit, pariatur. Inventore nostrum aliquam, ratione veniam possimus odio, iste, molestias, assumenda quia nemo recusandae. Accusamus optio dolore autem consequuntur molestiae ut aspernatur veniam. Placeat magnam numquam hic atque enim aliquam voluptatum totam! Debitis tempora necessitatibus totam incidunt alias labore quisquam, nam dolor dolore, maxime, nostrum, enim ut commodi nisi impedit. Optio aliquam minus, in fugit, et deleniti officia dolorum dolores quo blanditiis iusto doloremque.</p>
+                            <hr>
+                        </article>
+                        
+                        <article>
+                            <img src="img/img5.jpg" alt="Tattoo billede 2">
+                            <h3>Overskrift</h3>
+                            <p id="time">06. december 2016 11:12:37</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, fuga. Reprehenderit, pariatur. Inventore nostrum aliquam, ratione veniam possimus odio, iste, molestias, assumenda quia nemo recusandae. Accusamus optio dolore autem consequuntur molestiae ut aspernatur veniam. Placeat magnam numquam hic atque enim aliquam voluptatum totam! Debitis tempora necessitatibus totam incidunt alias labore quisquam, nam dolor dolore, maxime, nostrum, enim ut commodi nisi impedit. Optio aliquam minus, in fugit, et deleniti officia dolorum dolores quo blanditiis iusto doloremque.</p>
+                            <hr>
+                        </article>
+                        
+                        <article>
+                            <img src="img/img3.jpg" alt="Tattoo billede 3">
+                            <h3>Overskrift</h3>
+                            <p id="time">06. december 2016 11:12:37</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, fuga. Reprehenderit, pariatur. Inventore nostrum aliquam, ratione veniam possimus odio, iste, molestias, assumenda quia nemo recusandae. Accusamus optio dolore autem consequuntur molestiae ut aspernatur veniam. Placeat magnam numquam hic atque enim aliquam voluptatum totam! Debitis tempora necessitatibus totam incidunt alias labore quisquam, nam dolor dolore, maxime, nostrum, enim ut commodi nisi impedit. Optio aliquam minus, in fugit, et deleniti officia dolorum dolores quo blanditiis iusto doloremque.</p>
+                            <hr>
+                        </article>
+                        
+                        <article>
+                            <img src="img/img2.jpg" alt="Tattoo billede 4">
+                            <h3>Overskrift</h3>
+                            <p id="time">06. december 2016 11:12:37</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, fuga. Reprehenderit, pariatur. Inventore nostrum aliquam, ratione veniam possimus odio, iste, molestias, assumenda quia nemo recusandae. Accusamus optio dolore autem consequuntur molestiae ut aspernatur veniam. Placeat magnam numquam hic atque enim aliquam voluptatum totam! Debitis tempora necessitatibus totam incidunt alias labore quisquam, nam dolor dolore, maxime, nostrum, enim ut commodi nisi impedit. Optio aliquam minus, in fugit, et deleniti officia dolorum dolores quo blanditiis iusto doloremque.</p>
+                            <hr>
+                        </article>
+                        
+                        <article>
+                            <img src="img/img1.jpg" alt="Tattoo billede 5">
+                            <h3>Overskrift</h3>
+                            <p id="time">06. december 2016 11:12:37</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, fuga. Reprehenderit, pariatur. Inventore nostrum aliquam, ratione veniam possimus odio, iste, molestias, assumenda quia nemo recusandae. Accusamus optio dolore autem consequuntur molestiae ut aspernatur veniam. Placeat magnam numquam hic atque enim aliquam voluptatum totam! Debitis tempora necessitatibus totam incidunt alias labore quisquam, nam dolor dolore, maxime, nostrum, enim ut commodi nisi impedit. Optio aliquam minus, in fugit, et deleniti officia dolorum dolores quo blanditiis iusto doloremque.</p>
+                            <hr>
+                        </article>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="row">
+                            <div class="col-sm-6-offset-3 login-box">
+                                <div class="input-group">
+                                  <p>Brugernavn</p>
+                                  <input type="text" placeholder="Brugernavn" class="input">
+                                </div>
+                                <div class="input-group">
+                                  <p>Password</p>
+                                  <input type="text"placeholder="Password">
+                                </div>
+                                <button class="btn btn-default" type="button">Log ind</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    <!-- MAIN SLUT -->
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+      </body>
+</html>
+
+
